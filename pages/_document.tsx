ac1a437107c67b561a25a04fa83b76825d28045a
rohithqyrus <154348138+rohithqyrus@users.noreply.github.com>
2024-04-13 20:34:26 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import { CSPostHogProvider } from '@/components/providers';
 
 export default function Document() {
   return (
     <Html lang="en">
+      <CSPostHogProvider>
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
+    <CSPostHogProvider>
     </Html>
   )
 }
